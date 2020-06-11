b3d86303db17116b19613e094a842645ff2299d6
Afolabi Marvelous <afosmarvel@gmail.com>
2020-06-11 15:29:27 -0700

    Final changes

--- src/index.js
+++ src/index.js
@@ -11,3 +11,42 @@ $(function() {
         }
     })
 })
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+
+$('.carousel-testimonial').owlCarousel({
+  loop:true,
+  margin:0,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:3,
+          nav:true,
+          loop:false
+      }
+  }
+})
